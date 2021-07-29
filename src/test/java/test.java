24a4f860f804dba9ea1daf9cba739d1bb833aa2a
andrea aquino <andrea.aquino273@gmail.com>
2021-07-29 15:37:23 -0400

    primer test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class test {
+
+    @Test
+    public void getNumberTest() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assert.assertEquals(1, fizzBuzz.getNumber(1));
+    }
 }
